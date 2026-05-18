1d98d38eab7d76749042d9b77abc71d5bd65fb9c
roshann11 <roshanmandal2004.7@gmail.com>
2026-05-18 14:06:38 +0530

    landing and about page

---
+++ src/components/About.tsx
@@ -0,0 +1,25 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I am a full-stack engineer with a passion for building innovative
+          web applications. I have experience with a wide range of technologies,
+          including React, Node.js, and MongoDB. I am a quick learner and am
+          always looking to expand my skill set.
+        </p>
+        <p className="para">
+          In my free time, I enjoy working on personal projects, contributing
+          to open source, and staying up-to-date with the latest trends in
+          web development.
+          full-stack products end to end.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Landing.tsx
@@ -0,0 +1,35 @@
+import { PropsWithChildren } from "react";
+import "./styles/Landing.css";
+
+const Landing = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <div className="landing-section" id="landingDiv">
+        <div className="landing-container">
+          <div className="landing-intro">
+            <h2>Hello! I'm</h2>
+            <h1>
+              Roshan
+              <br />
+              <span>Mandal</span>
+            </h1>
+          </div>
+          <div className="landing-info">
+            <h3>Student &</h3>
+            <h2 className="landing-info-h2">
+              <div className="landing-h2-1">Full Stack</div>
+              <div className="landing-h2-2">AI</div>
+            </h2>
+            <h2>
+              <div className="landing-h2-info">Developer</div>
+              <div className="landing-h2-info-1">Engineer</div>
+            </h2>
+          </div>
+        </div>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Landing;
